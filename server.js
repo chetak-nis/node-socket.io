1856be07b59d6923e97cf85f2dee728dfa902c21
Chetak <chetak@nichi.com>
2018-12-13 10:05:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('alert', (data) => io.emit('alert', data));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
